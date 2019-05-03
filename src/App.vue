2e4069d7268f3e819c1822c46db49c507d54888a
mcueto <cueto@live.cl>
2019-05-02 23:17:36 -0400

    Create Navbar, Section & SectionGallery components with dummy data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar class="mb-4" />
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 offset-md-0">
+          <h1>
+            Comunicación Digital
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 offset-md-0">
+          <Section v-for="section in sections" v-bind:key="section.id" v-bind:title="section.name" v-bind:description="section.description" />
+        </div>
+      </div>
+
+    </div>
+
+    <div class="container text-center my-4 py-4">
+      <p>
+        Sitio creado por <a href="#">agapanto comunicacion digital</a>
+      </p>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Section from './components/Section.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'app',
+  data: function () {
+    return {
+      sections: [
+          {
+            "name": "Sitios web",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    Section,
+    Navbar,
   }
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-branding">
+    <div class="container">
+      <a class="navbar-brand logo" href="{% url 'index' %}">
+        agapanto
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="far fa-user"></i> username
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <!-- <a class="dropdown-item" href="#">
+                  <i class="fas fa-user"></i> Edit account
+                </a>
+                <div class="dropdown-divider"></div> -->
+                <a class="dropdown-item" href="">
+                  <i class="fas fa-sign-out-alt"></i> Logout
+                </a>
+              </div>
+            </li>
+
+          </ul>
+
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+  props: {
+  },
+  components: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
