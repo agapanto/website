d60f0023b300c53522a633074d7be9cf82e1f91b
mcueto <cueto@live.cl>
2019-05-14 23:12:14 -0400

    Create PortfolioItem
    - Rename SectionGallery to PortfolioItem
    - props with default and required=false
    - html reduced
    - update blank_image.png asset

---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="col-12 col-md-4 my-2 my-md-3">
+    <div class="gallery-shadow">
+      <a v-bind:href='url' target="_blank">
+        <img v-bind:alt='description' v-bind:src='image' class="img-fluid">
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioItem',
+  props: {
+    name: String,
+    description: String,
+    image: {
+        type: String,
+        required: false,
+        default: '../assets/blank_image.png'
+    },
+    url: {
+        type: String,
+        required: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
